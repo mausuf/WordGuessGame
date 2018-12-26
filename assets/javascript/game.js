1b749f63e0392d06e22a24d532ca2e9625496c68
starbuck <mausuf@utexas.edu>
2018-12-26 15:53:24 -0600

    js setup update1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,18 @@
-var game = ["TREE", "APPLE", "GREEN"];
-var choice = Math.floor(Math.random()*3);
-var answer = game[choice];
+var computerWords = ["BONE", "APPLE", "TEA"];
+var blankLines = [];
+var test = "testing"
+var blanks = "_"
 
-alert(answer)
+
 //test
 
-//more testing
+var computerGuess = computerWords[Math.floor(Math.random()* computerWords.length)];
+for (i=0; i<test.length; i++) {
+    blankLines.push(blanks);
+    alert(blankLines);};
+
+    document.getElementById("112").innerHTML = blankLines
+   
+
+
+var userGuess = event.key;
